75f407cada598397feba0aa1762d47f1439edeb6
davidiles <36132264+davidiles@users.noreply.github.com>
2018-06-04 13:47:40 -0400

    age classes respond similarly

--- fish_pop_model3_constrained.R
+++ fish_pop_model3_constrained.R
@@ -1,4 +1,100 @@
+library(jagsUI)
+library(pscl)
+library(ggplot2)
 
+rm(list=ls())
+
+###########################################
+# Read in trawl data
+###########################################
+yc=read.csv("../../data/CPUE_ycs_trawl_withcobb_BLS_2017.csv",header=TRUE)
+head(yc)
+
+###########################################
+# Reorganize
+########################################### 
+yc$logoffset=log(yc$numtrawlyr)
+head(yc)
+names(yc)
+
+##fixed effects
+yc$fyear=as.factor(yc$year_cap)
+yc$flocation=as.factor(yc$location)
+yc$fdepth=as.factor(yc$trawldepthft)
+
+#Check out patterns in raw data
+rawdata_plot = ggplot(data = yc, aes(x = year_cap, y = countinyear, col = fdepth, shape = fdepth)) +
+  geom_jitter(width = 0.2, height = 0)+
+  facet_grid(assnage~., scales = "free")+
+  theme_bw()
+print(rawdata_plot)
+
+# Create dummy variables for fdepth
+yc$fdepth60 = yc$fdepth90 = yc$fdepth120 = 0
+yc$fdepth60[yc$fdepth == "60"] = 1
+yc$fdepth90[yc$fdepth == "90"] = 1
+yc$fdepth120[yc$fdepth == "120"] = 1
+
+# Create empty array to store age-specific abundances each year
+obs_year_age = table(yc[,c("year_cap","assnage")]) #number of observations per age class per year
+years = min(yc$year_cap):max(yc$year_cap)
+years = c(years[1] - 1, years)
+
+### Array to store counts each year
+# First dimension of array is years of study
+# Second dimension of array is age classes
+# Third dimension of array is repeated counts for that age in that year (currently, up to 17)
+N_array = array(NA, dim = c(length(years), length(unique(yc$assnage)), max(obs_year_age)))
+
+# Arrays to store covariates
+yc$fdepth_fac = as.numeric(as.factor(yc$fdepth))
+fdepth_array = N_array
+
+for (y in unique(yc$year_cap)){
+  for (age in unique(yc$assnage)){
+    
+    year_age_dat = subset(yc, year_cap == y & assnage == age)
+    
+    for (i in 1:nrow(year_age_dat)){
+      array_dim1 = which(years == y)
+      array_dim2 = which(sort(unique(yc$assnage)) == age)
+      array_dim3 = i
+      
+      N_array[array_dim1, array_dim2, array_dim3] = year_age_dat$countinyear[i]
+      fdepth_array[array_dim1, array_dim2, array_dim3] = year_age_dat$fdepth_fac[i]
+      
+      
+    }
+  }
+}
+
+#Provide fdpeth in years when no observations were recorded (this does not affect results)
+#Analysis requires covariates for every observation (including missing observations)
+fdepth_array[is.na(fdepth_array)] = 1 
+
+#Check out observed counts for each year and age class
+N_array[1,,] #Year 1 (2000); 17 obs for each age class
+N_array[2,,] #Year 2 (2001); no data
+N_array[3,,] #Year 3 (2002); 17 obs for each age class
+N_array[4,,] #Year 4 (2003); no data
+N_array[5,,] #Year 5 (2004); 16 obs for each age class
+N_array[6,,] #Year 6 (2005); no data
+N_array[7,,] #Year 7 (2006); 13 obs for each age class
+N_array[8,,] #Year 8 (2007); no data
+N_array[9,,] #Year 9 (2008); 17 obs for each age class
+N_array[10,,] #Year 10 (2009); no data
+N_array[11,,] #Year 11 (2010); 6 obs for each age class
+N_array[12,,] #Year 12 (2011); no data
+N_array[13,,] #Year 13 (2012); 13-15 obs for each age class
+N_array[14,,] #Year 14 (2013); no data
+N_array[15,,] #Year 15 (2014); 17 obs for each age class
+N_array[16,,] #Year 16 (2015); 10 obs for each age class
+N_array[17,,] #Year 17 (2016); 4 obs for each age class
+N_array[18,,] #Year 18 (2017); 6 obs for each age class
+
+# Specify model in JAGS language
+sink("fish_pop_model.txt")
+cat("
     model{
     
     #-----------------------------------------
@@ -231,4 +327,106 @@
     
     
     }
-    
+    ",fill = TRUE)
+sink()
+
+# Bundle data
+jags.data <- list(N = N_array,
+                  Y = dim(N_array)[1],
+                  A = dim(N_array)[2],
+                  O = dim(N_array)[3],
+                  fdepth = fdepth_array)
+
+# Initial values
+inits <- function() list(zero = array(0,dim = dim(N_array))#,r = c(0.35, 1.2, 1.2, 0.55, 0.55)
+                         )
+
+# Parameters monitored
+params <- c("r","zero.B0","beta.fdepth",
+            
+            "log.reprod.2.intercept","log.reprod.3.adjust","log.reprod.4.adjust",
+            "log.reprod.2.sd",
+            
+            "logit.surv.0.intercept","logit.surv.1.adjust","logit.surv.2.adjust","logit.surv.3.adjust",
+            "logit.surv.0.sd",
+            
+            "log.reprod.resid",
+            "logit.surv.resid",
+            
+            "reprod.2","reprod.3","reprod.4",
+            "surv.0","surv.1","surv.2","surv.3",
+            "mu.count"
+)
+
+# MCMC settings
+ni <- 50000
+nt <- 2
+nb <- 45000
+nc <- 2
+
+# Call JAGS from R
+out <- jags(data = jags.data, 
+            inits, 
+            params, 
+            model.file = "fish_pop_model.txt", 
+            n.chains = nc, 
+            n.thin = nt, 
+            n.iter = ni, 
+            n.burnin = nb)
+
+out
+
+mu.1.med = apply(out$sims.list$mu.count[,,1],2,function(x) quantile(x,0.500))
+mu.1.lcl = apply(out$sims.list$mu.count[,,1],2,function(x) quantile(x,0.025))
+mu.1.ucl = apply(out$sims.list$mu.count[,,1],2,function(x) quantile(x,0.975))
+
+mu.2.med = apply(out$sims.list$mu.count[,,2],2,function(x) quantile(x,0.500))
+mu.2.lcl = apply(out$sims.list$mu.count[,,2],2,function(x) quantile(x,0.025))
+mu.2.ucl = apply(out$sims.list$mu.count[,,2],2,function(x) quantile(x,0.975))
+
+mu.3.med = apply(out$sims.list$mu.count[,,3],2,function(x) quantile(x,0.500))
+mu.3.lcl = apply(out$sims.list$mu.count[,,3],2,function(x) quantile(x,0.025))
+mu.3.ucl = apply(out$sims.list$mu.count[,,3],2,function(x) quantile(x,0.975))
+
+mu.4.med = apply(out$sims.list$mu.count[,,4],2,function(x) quantile(x,0.500))
+mu.4.lcl = apply(out$sims.list$mu.count[,,4],2,function(x) quantile(x,0.025))
+mu.4.ucl = apply(out$sims.list$mu.count[,,4],2,function(x) quantile(x,0.975))
+
+mu.5.med = apply(out$sims.list$mu.count[,,5],2,function(x) quantile(x,0.500))
+mu.5.lcl = apply(out$sims.list$mu.count[,,5],2,function(x) quantile(x,0.025))
+mu.5.ucl = apply(out$sims.list$mu.count[,,5],2,function(x) quantile(x,0.975))
+
+p1 = ggplot() +
+  geom_ribbon(aes(x = years, ymin = mu.1.lcl, ymax = mu.1.ucl), alpha = 0.2) +
+  geom_line(aes(x = years, y = mu.1.med)) +
+  ylab("Rel. abundance")+
+  theme_bw()
+
+p2 = ggplot() +
+  geom_ribbon(aes(x = years, ymin = mu.2.lcl, ymax = mu.2.ucl), alpha = 0.2) +
+  geom_line(aes(x = years, y = mu.2.med)) +
+  ylab("Rel. abundance")+
+  theme_bw()
+
+p3 = ggplot() +
+  geom_ribbon(aes(x = years, ymin = mu.3.lcl, ymax = mu.3.ucl), alpha = 0.2) +
+  geom_line(aes(x = years, y = mu.3.med)) +
+  ylab("Rel. abundance")+
+  theme_bw()
+
+p4 = ggplot() +
+  geom_ribbon(aes(x = years, ymin = mu.4.lcl, ymax = mu.4.ucl), alpha = 0.2) +
+  geom_line(aes(x = years, y = mu.4.med)) +
+  ylab("Rel. abundance")+
+  theme_bw()
+
+p5 = ggplot() +
+  geom_ribbon(aes(x = years, ymin = mu.5.lcl, ymax = mu.5.ucl), alpha = 0.2) +
+  geom_line(aes(x = years, y = mu.5.med)) +
+  ylab("Rel. abundance")+
+  theme_bw()
+
+library(cowplot)
+
+fullplot = plot_grid(p1,p2,p3,p4,p5, nrow=5)
+print(fullplot)
